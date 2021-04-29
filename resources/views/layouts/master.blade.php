c06eac41f84e095abe4a55ea73eabde3a26681ad
Mark <mark.graham004@gmail.com>
2021-04-29 13:13:03 +0100

    Changed bootstrap links from Bootstrap3 to Bootstrap4

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,13 +8,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- Bootstrap Stylesheet -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
     <!-- Our Stylesheet -->
     <link rel="stylesheet" href="{{ asset('css/site.css') }}" />
     
     <!-- Bootstrap Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- Our Scripts -->
     <script src="{{ asset('js/site.js') }}"></script>
 
