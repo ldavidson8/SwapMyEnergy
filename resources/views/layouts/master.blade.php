93325e24fa6b69178d649a3ede277ac42d9696fa
Mark <mark.graham004@gmail.com>
2021-04-29 13:56:24 +0100

    Changed the navbar appearance

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,20 +21,20 @@
 </head>
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-    </div>
-  </div>
-</nav>
+        <div class="col-md-12">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <a class="nav-item nav-link active" href="{{ route('login') }}">Home <span class="sr-only"></span></a>
+                        <a class="nav-item nav-link active" href="{{ route('login') }}">Login</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
     </header>
 
     <main class="col-md-12">
--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,26 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::prefix('accounts')->group(function () {
+Route::group([ 'prefix' => 'account' ], function()
+{
     Route::get('login', function()
     {
-        return view('accounts.login');
-    });
+        return view('account.login');
+    }) -> name('login');
 
     Route::post('login', function()
     {
         
     });
-    
+
 
     Route::get('sign-up', function()
     {
-        return view('accounts.sign-up');
-    });
+        return view('account.sign-up');
+    }) -> name('sign-up');
 
     Route::post('sign-up', function()
     {
-        return view('accounts.sign-up');
+        return view('account.sign-up');
     });
 });
