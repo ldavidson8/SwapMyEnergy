187b8930c1c84535712f39c64df4ff310a431bda
Lewis <lewisdavidsonznergi@gmail.com>
2021-05-05 14:33:18 +0100

    second section of Partners and Affiliates

--- resources/views/other/partners-and-affiliates.blade.php
+++ resources/views/other/partners-and-affiliates.blade.php
@@ -16,6 +16,24 @@
                 margin: 2%;
             }
         }
+
+
+        .center-div-outer
+        {
+            position: relative;
+        }
+
+        .center-div
+        {
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 0;
+            right: 0;
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+            padding: 10px;
+        }
     </style>
 @endsection
 
@@ -35,32 +53,50 @@
    </div>
    <hr/>
 
-<div class="contianer-fluid" style="background-color: white;">
-    <div class="container fixed-width">
-    <div class="row" style="padding: 30px 0px;">
-            <div class="col-6" style="text-align: right; border-right: 3px solid black;">
-                <h2> Your Benefits </h2>
-                <ul style="line-height: 1.2;">
-                    <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</li>
-                    <li>Incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation.</li>
-                    <li>Ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit.</li>
-                    <li>In voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatate.</li>
-                </ul>
-            </div>
-            <div class="col-6" style="text-align: left;">
-                <h2 style="font-size: 45px;"> Join Our Family</h2>
-                <button type="button" class="bigbluebutton btn-lg">Join Us</button> 
+    <div class="contianer-fluid" style="background-color: white;">
+        <div class="container fixed-width">
+            <div class="row" style="padding: 30px 0px;">
+                <div class="col-6" style="text-align: right; border-right: 3px solid black;">
+                    <h2> Your Benefits </h2>
+                    <ul style="line-height: 1.2;">
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</li>
+                        <li>Incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation.</li>
+                        <li>Ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit.</li>
+                        <li>In voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatate.</li>
+                    </ul>
+                </div>
+                <div class="col-6" style="text-align: left;">
+                    <h2 style="font-size: 45px;"> Join Our Family</h2>
+                    <button type="button" class="bigbluebutton btn-lg">Join Us</button> 
+                </div>
             </div>
         </div>
     </div>
     <hr/>
     
-<div class="full-size container-fluid d-flex h-100 flex-column">
-<main class="row flex-grow-1" style="padding: 0px;">
-        <div class="md-6">
-            <h2> Your Benefits </h2>
-        </div>
-        <div class="md-6">
-        </div>
-</div>
+    <div class="full-size container-fluid d-flex h-100 flex-column">
+        <main class="row flex-grow-1" style="padding: 0px;">
+            <div class="col-lg-6 col-12 center-div-outer">
+                <div class="center-div">
+                    <div style="text-align: right; width: 50%; display: inline-block; float: left; padding-right: 20px; border-right: 3px solid black;">
+                        <h2> Your Benefits </h2>
+                        <ul style="line-height: 1.2;">
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</li>
+                            <li>Incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation.</li>
+                            <li>Ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit.</li>
+                            <li>In voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatate.</li>
+                        </ul>
+                    </div>
+                    <div style="text-align: left; width: 50%; display: inline-block; float: right; padding-left: 20px;">
+                        <h2 style="font-size: 45px;">Join Our Family</h2>
+                        <button type="button" class="bigbluebutton btn-lg">Join Us</button> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-12" style="background-color: white;">
+
+            </div>
+        </main>
+    </div>
+    
 @endsection()
