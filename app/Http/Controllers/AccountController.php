5a5c7054ec4e2df19e82e3b4d56d18928c71f0e6
Mark <mark.graham004@gmail.com>
2021-05-05 15:22:13 +0100

    Miscelaneous Changes

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,28 +7,6 @@ use App\Models\HandleSessions;
 
 class AccountController extends Controller
 {
-    public function login(Request $request)
-    {
-        return view('account.login', [ 'request' => $request, 'navbar_page' => 'login' ]);
-    }
-    
-    public function loginPost(Request $request)
-    {
-        
-    }
-
-    public function logout()
-    {
-        
-    }
-
-
-    public function registerPost(Request $request)
-    {
-        
-    }
-
-
     public function myAccount(Request $request)
     {
         return view('account.my-account', [ 'request' => $request, 'navbar_page' => 'my account' ]);
