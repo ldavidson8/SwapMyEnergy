90f51a8c89dd5403aff54f3b7a0b4881d02ddfd0
Mark <mark.graham004@gmail.com>
2021-05-06 12:47:59 +0100

    Footer and Header Style Changes

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,17 +4,17 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-outer">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
     
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav mr-auto" style="display: flex; align-items: center; font-size: 22px; font-">
                 <li class="nav-item">
-                    <a class="nav-link navigation-link {{ ($navbar_page == "home") ? 'navigation-link-current-page' : '' }}" href="{{ route('home') }}">Home</a>
-                </li>
+                    <a class="nav-link navigation-link {{ ($navbar_page == "home") ? 'navigation-link-current-page"' : '' }}" href="{{ route('home') }}">Home</a>
+                </li> |
                 <li class="nav-item">
                     <a class="nav-link navigation-link {{ ($navbar_page == "about") ? 'navigation-link-current-page' : '' }}" href="{{ route('about') }}">About</a>
-                </li>
+                </li> |
                 <li class="nav-item">
                     <a class="nav-link navigation-link {{ ($navbar_page == "my account") ? 'navigation-link-current-page' : '' }}" href="{{ route('my account') }}">My Account</a>
                 </li>
