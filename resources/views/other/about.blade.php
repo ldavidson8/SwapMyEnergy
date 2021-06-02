5604699462ad1100953aeaf31ea5603ded8a0ca7
Lewis <lewisdavidsonznergi@gmail.com>
2021-06-02 14:47:54 +0100

    Hidden Our Team section

--- resources/views/other/about.blade.php
+++ resources/views/other/about.blade.php
@@ -84,7 +84,7 @@
             </div>
         </div>
     </div>
-    <div class="full-size-50 container-fluid d-flex flex-column">
+    {{-- <div class="full-size-50 container-fluid d-flex flex-column">
         <div class="row flex-grow-1 no-padding" style="background-color: #f3f2f1;">
             <div class="col-xl-1 col-lg-1 col-md-2 d-none d-md-block"></div>
             <div class="col-xl-4 col-lg-5 col-md-8 col-12 left-column-content align-items-center mobile-only-padding-30" style="text-align: left;">
@@ -94,5 +94,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
