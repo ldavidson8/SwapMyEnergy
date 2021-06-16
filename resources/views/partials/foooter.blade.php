5a6344384a64b8e50254e1e67b0c8ec1cdfc2c96
Mark <mark.graham004@gmail.com>
2021-06-16 12:53:07 +0100

    Added rel=external to footer links

--- resources/views/partials/foooter.blade.php
+++ resources/views/partials/foooter.blade.php
@@ -23,10 +23,10 @@
                 <div style="text-align: left; font-size: 16px; border-bottom: 3px solid #f3f2f1;">
                     <p> Swap My Energy is an energy intermediary and not supplier <br /> The UK trading name follows the Utilities Intermediaries Association code of conduct and Ofgem policies. All suppliers logos are trademarks of their respective owners. Percentage energy savings quoted are against customers who let their last contract renew automatically. </p>
                 </div>
-                <a href="https://www.znergi.co.uk/">
+                <a href="https://www.znergi.co.uk/" rel="external">
                     <img class="default" src="{{ asset('img/Znergi_logo.png')}}" style="vertical-align:top; float: left; margin: 20px 30px 0 0;">
                 </a>
-                <p style="text-align: left;"> 2nd Floor Estate House, <br /> Fox Street, Preston, PR1 2AB <br /> 01772 584880 (Option 1) <br /> <a style="color: #f3f2f1; text-decoration: underline;" href="https://www.znergi.co.uk/"> www.znergi.co.uk </a></p>
+                <p style="text-align: left;"> 2nd Floor Estate House, <br /> Fox Street, Preston, PR1 2AB <br /> 01772 584880 (Option 1) <br /> <a style="color: #f3f2f1; text-decoration: underline;" href="https://www.znergi.co.uk/" rel="external"> www.znergi.co.uk </a></p>
             </div>
             <div class="col-12 col-lg-4 order-1 order-lg-2 center-content">
                 <table id="footer-links" class="mb-3 mb-lg-0">
@@ -53,22 +53,22 @@
                 <div style="text-align: center; margin: auto; padding: 10px; font-size: 16px;">
                     <p> Swap My Energy is a trading name of Znergi Limited in England, Company Registration Number 12937329</p>
                     <p> Copyright &copy; 2021 Znergi Limited All rights reserved </p>
-                    <a class="footer-social-link" href="https://www.facebook.com/Swap-My-Energy-101013942165296">
+                    <a class="footer-social-link" href="https://www.facebook.com/Swap-My-Energy-101013942165296" rel="external">
                     <img class="footer-social-link-image" src="{{ asset('img/social-links/facebook.svg') }}" width="50" height="auto" />
                     </a>
-                    <a class="footer-social-link" href="https://twitter.com/SwapMyEnergy">
+                    <a class="footer-social-link" href="https://twitter.com/SwapMyEnergy" rel="external">
                     <img class="footer-social-link-image" src="{{ asset('img/social-links/twitter.svg') }}" width="50" height="auto"/>
                     </a>
-                    <a class="footer-social-link" href="https://www.instagram.com/swapmyenergy/">
+                    <a class="footer-social-link" href="https://www.instagram.com/swapmyenergy/" rel="external">
                     <img class="footer-social-link-image" src="{{ asset('img/social-links/instagram.svg') }}" width="50" height="auto"/>
                     </a>
-                    <a class="footer-social-link" href="https://www.tiktok.com/@swapmyenergy">
+                    <a class="footer-social-link" href="https://www.tiktok.com/@swapmyenergy" rel="external">
                     <img class="footer-social-link-image" src="{{ asset('img/social-links/tiktok.svg') }}" width="50" height="auto"/>
                     </a>
-                    <a class="footer-social-link" href="https://www.linkedin.com/company/77837475/admin/">
+                    <a class="footer-social-link" href="https://www.linkedin.com/company/77837475/admin/" rel="external">
                     <img class="footer-social-link-image" src="{{ asset('img/social-links/linkedin.svg') }}" width="50" height="auto"/>
                     </a>
-                    <a class="footer-social-link" href="https://www.youtube.com/channel/UC7u_949FAQeV9FlZm4dH7lQ">
+                    <a class="footer-social-link" href="https://www.youtube.com/channel/UC7u_949FAQeV9FlZm4dH7lQ" rel="external">
                     <img class="footer-social-link-image" src="{{ asset('img/social-links/youtube.svg') }}" width="50" height="auto"/>
                     </a>
                 </div>
