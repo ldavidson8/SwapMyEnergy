235a0204df357500a21d638e53c2c5953d2c1ede
Lewis <lewisdavidsonznergi@gmail.com>
2021-07-08 16:27:30 +0100

    Audited text and changed Residential home bg img for text clarity on mobile

--- resources/views/index/residential.blade.php
+++ resources/views/index/residential.blade.php
@@ -125,11 +125,11 @@
 
 @section('main-content')
         <hr/>
-        <div class="row flex-grow-1 no-padding background-image-wind-turbines">
+        <div class="row flex-grow-1 no-padding background-image-wind-turbines background-image-left">
             <div class="col-xl-2 col-lg-1 col-md-2 d-none d-md-block"></div>
             <div class="col-xl-3 col-lg-6 col-md-8 col-12 center-content">
                 <h1>Don't fall victim to rising energy prices</h1>
-                <p>Too many energy brokers promise savings that don’t stick. These promises are made on estimates, which aren’t always respective of the energy that you use. At Swap My Energy, we give you savings based on facts, not estimates, providing you with the transparency you deserve when it comes to your bills.</p>
+                <p>Too many energy brokers promise savings that do not stick. These promises are made on estimates, which aren not always accurate of the energy that you use. At Swap My Energy, we give you savings based on facts, not estimates, providing you with the transparency you need when it comes to your bills.</p>
                 <a href="{{ route('residential.energy-comparison.1-address') }}" class="btn big-blue-button" role="button" style="margin-top: 10px;">Get started</a>
             </div>
             <div class="col-xl-7 col-lg-5 col-md-2 d-none d-md-block">
@@ -145,7 +145,7 @@
                     <div style="text-align: center;">
                         <img src="{{ asset('img/infographic icons/signed-form.svg') }}"/>
                         <p class="infographics-header"> Fill in our form </p>
-                        <p> Our form is easy to understand and will help you get the best deals available to you </p>
+                        <p> It's easy to understand and will help you get the best deals you can </p>
                     </div>
                 </div>
             </div>
@@ -154,7 +154,7 @@
                     <div style="text-align: center;">
                         <img src="{{ asset('img/infographic icons/search-icon.svg') }}"/>
                         <p class="infographics-header"> Browse our deals </p>
-                        <p> You'll have all the best deals presented to you, allowing you to easily understand which deal best suits you.  </p>
+                        <p>We show you the best deals first, letting you see what you are saving per unit</p>
                     </div>
                 </div>
             </div>
@@ -164,7 +164,7 @@
                     <div style="text-align: center;">
                         <img src="{{ asset('img/infographic icons/switch-icon.svg') }}"/>
                         <p class="infographics-header"> Get switching </p>
-                        <p> Once you're sure that you got the best deal for you, toggle the switch and leave the rest up to us. </p>
+                        <p> Once you have the best deal for you, click get switching</p>
                     </div>
                 </div>
             </div>
@@ -177,7 +177,7 @@
             <div class="col-xl-4 col-lg-5 col-md-8 col-12 left-column-content align-items-center mobile-only-padding-30" style="text-align: left;">
                 <div>
                     <h2 style="font-size: 44px"> How it works </h2>
-                    <p style="padding-top: 15px">Other energy brokers promise false savings based on their estimates, which can fall short of your actual usage, which can result in steep final bills and hassle that you just don't need. Our savings are calculated based on the unit price, which means that a lower unit price plan will result in savings for you or some room for more brews or a new TV to binge-watch Netflix on. </p>
+                    <p style="padding-top: 15px">Other energy brokers promise false savings based on their estimates, which can fall short of your actual usage. This can result in steep final bills and hassle that you just don't need. Our savings are calculated based on the unit price, which means that a lower unit price plan will result in savings for you or some room for more brews or a new TV to binge-watch on. </p>
                 </div>
             </div>
             <div class="col-2 d-lg-none d-block"></div>
@@ -203,20 +203,20 @@
                     <h2 style="text-align: center;">Frequently Asked Questions</h2>
                     <button class="faq-accordion">Why choose Swap My Energy?</button>
                     <div class="faq-panel">
-                        <p>Swap My Energy are dedicated to making switching simple. We allow you to see as much information regarding the deals available to you as possible, allowing you to gain a full understanding on why the prices are the way they are.</p>
+                        <p>Swap My Energy are dedicated to switching made simple. We allow you to see as much information regarding your deals as possible, allowing you to know exactly why your prices are the way they are.</p>
                     </div>
                     <button class="faq-accordion">How long does it take to switch?</button>
                     <div class="faq-panel">
-                        <p>Once you've decided to switch, your information will be provided to the supplier of your choice and your switching will be underway. Typically, suppliers take around 2 weeks to switch you over, this is because you have the option within the 2 weeks to stop switching.</p>
+                        <p>Once you have decided to switch, your information will be provided to the supplier of your choice and your switching will be underway. Typically, suppliers take around two weeks to switch you over, this gives you a chance to change your mind and stop switching.</p>
                     </div>
 
                     <button class="faq-accordion">Why do you need my suppliers name and my current usage?</button>
                     <div class="faq-panel">
-                        <p>This helps us understand the deal you were on and to help calculate the prices of the deals based on your current usage.</p>
+                        <p>This allows us to understand your current tariffs and to help calculate the deals based on your current usage</p>
                     </div>
                     <button class="faq-accordion">Will you always get me a better deal?</button>
                     <div class="faq-panel">
-                        <p>No. Some other energy switching companies will promise you a better deal, by "estimating" your usage for the next year. We let you know your unit price and standing charges, allowing you to understand how much it costs you per kW. Our aim to get you a better deal based on unit price and standing charges, meaning that if you get lower prices on these, then you'll end up paying less (assuming that your usage is the same). <br /> This works in a similar way to when you're putting fuel in your car. If one week you buy 50L of fuel at &pound;1.35 per litre and then the next week at &pound;1.25 per litre, you'll save money. But if you buy 75L of fuel at &pound;1.25 instead, you'll end up spending more</p>
+                        <p>No. Some other energy switching companies will promise you a better deal, by "estimating" your usage for the next year. We let you know your unit price and standing charges, allowing you to understand how much it costs you per kW. Our aim is to get you a better deal based on unit price and standing charges, this means that if you get lower prices on these, then you'll end up paying less (assuming that your usage is the same). This works in a similar way to when you're putting fuel in your car. If one week you buy 50L of fuel at &pound;1.35 per litre and then the next week at &pound;1.25 per litre, you will save money. But if you buy 75L of fuel at £1.25, you'll end up spending more. </p>
                     </div>
                     <button class="faq-accordion">Can I opt-out of switching?</button>
                     <div class="faq-panel">
