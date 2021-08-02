eaabf5c7df9ffa998571d36ab298f5da45542f18
Shaesgithub <84018333+Shaesgithub@users.noreply.github.com>
2021-08-02 12:43:31 +0100

    table added to error pages

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -12,25 +12,36 @@
 @endsection
 
 @section('main-content')
-    <hr/>
-    <div class="row flex-grow-1 no-padding">
-        <div class="col-xl-1 col-lg-1 col-md-2 d-none d-md-block"></div>
-        <div class="col-xl-5 col-lg-6 col-md-8 col-12 left-column-content" style="background-position: left; background: #f0f0f0;">
-            <h1 style="font-weight: bold; font-size: 40px; color: #000; text-align: center; letter-spacing: -3px;"><span style="letter-spacing: 3px; font-size: 90px;color: #00C2CB;">429</span><br />There Are Too Many Requests</h1>
-            <br /><br /><br />
-            <p style="color: #000; text-align: center; font-size: 18px;letter-spacing: 1px;">Too much <span style=" color: #00C2CB;">traffic is being generated</span><br />Please try again later<p>
-            <br />
-            <br />
-            <div style="display: flex; justify-content: center; align-items: center;">
-                <p><a href="{{ url() -> previous()}}" onclick="history.back()"><button class="big-blue-button" style="width: 300px; font-size: 23px;">Go Back</button></a></p>
+        <hr/>
+        <div class="flex-grow-1 row flex-grow-1 no-padding">
+            <div class="col-xl-1 col-lg-1 col-md-2 d-none d-md-block"></div>
+            <div class="col-xl-5 col-lg-6 col-md-8 col-12" style="background: #f0f0f0; background-position: left;">
+                <table class="table-center-contents">
+                    <tr>
+                        <td>
+                            <h1 style="font-weight: bold; font-size: 40px; color: #000;  text-align: center; letter-spacing: -3px;"><span style="letter-spacing: 3px; font-size: 100px; color: #00C2CB;">429</span><br />There Are Too Many Requests<h1>
+                            <br />
+                            <div style="color: #000; text-align: center; font-size: 18px; letter-spacing: 1px;">
+                                <p>Too much <span style=" color: #00C2CB;">traffic is being generated</span><br />Please try again later<p>
+                                <br />
+                                <p>You can contact us using the details below.</p>
+                                <p>
+                                    Telephone: {{ env('DATA_CONTACT_PHONE_NUMBER') }}<br />
+                                    Email: {{ env('DATA_CONTACT_EMAIL') }}.
+                                </p>
+                                <br />
+                                <div style="display: flex; justify-content: center; align-items: center;">
+                                    <p><a href="{{ url() -> previous()}}" onclick="history.back()"><button class="big-blue-button" style="width: 300px; font-size: 23px;">Go Back</button></a></p>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
             </div>
-            <br />
-            <br />
+            <div class="col-xl-6 col-lg-5 col-md-2 background-image-display-none" style="background-image: url('{{ asset('img/error pages/429.png') }}'); background-repeat: no-repeat; background-position: center; background-size: contain; background-origin: content-box; padding: 50px; opacity: 0.8;"></div>
         </div>
-        <div class="col-xl-6 col-lg-5 col-md-2 background-image-display-none" style="background-image: url('{{ asset('img/error pages/429.png') }}'); background-repeat: no-repeat; background-position: center; background-size: contain; background-origin: content-box; padding: 50px; opacity: 0.8;"></div>
     </div>
 @endsection
 
 @section('after-footer')
-    </div>
 @endsection
