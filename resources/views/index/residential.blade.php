adf230128800f4e4735f2674f75b4548a136a862
Lewis <lewisdavidsonznergi@gmail.com>
2021-08-18 13:54:16 +0100

    Testing google adsense banner

--- resources/views/index/residential.blade.php
+++ resources/views/index/residential.blade.php
@@ -259,6 +259,16 @@
             </div>
         </div>
     </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7935793974309189"
+     crossorigin="anonymous"></script>
+    <!-- faq section -->
+    <ins class="adsbygoogle"
+        style="display:inline-block;width:728px;height:90px"
+        data-ad-client="ca-pub-7935793974309189"
+        data-ad-slot="4815113020"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 @endsection
 
 @section('script')
