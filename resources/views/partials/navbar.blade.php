c01661e84ecf31bf620a45c1a4c4f0bebbcc61c7
Lewis <lewisdavidsonznergi@gmail.com>
2021-08-25 15:05:41 +0100

    Navbar dropdown for services and payments route

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,28 +1,36 @@
 <?php
     if (!isset($navbar_page)) $navbar_page = "";
 ?>
-<nav class="navbar navbar-expand-md navbar-light navbar-outer">
+<nav class="navbar navbar-expand-lg navbar-light navbar-outer">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse mt-5 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto" style="display: flex; align-items: center; font-size: 22px;">
                 <li class="nav-item">
                     <a class="nav-link navigation-link {{ ($navbar_page == "home") ? 'navigation-link-current-page' : '' }}" href="{{ route("$mode.home") }}">Home</a>
                 </li>
-                <div class="d-lg-inline d-none" style="font-weight: 700;">|</div>
+                <div class="d-xl-inline d-none" style="font-weight: 700;">|</div>
                 <li class="nav-item">
                     <a class="nav-link navigation-link {{ ($navbar_page == "about") ? 'navigation-link-current-page' : '' }}" href="{{ route("$mode.about") }}">About</a>
                 </li>
-                <div class="d-lg-inline d-none" style="font-weight: 700;">|</div>
+                <div class="d-xl-inline d-none" style="font-weight: 700;">|</div>
                 <li class="nav-item">
                     <a class="nav-link navigation-link {{ ($navbar_page == "our team") ? 'navigation-link-current-page' : '' }}" href="{{ route("$mode.our-team") }}">Our Team</a>
                 </li>
-                <div class="d-lg-inline d-none" style="font-weight: 700;">|</div>
-                <li class="nav-item">
+                <div class="d-xl-inline d-none" style="font-weight: 700;">|</div>
+                <li class="nav-item d-none d-xl-inline">
                     <a class="nav-link navigation-link {{ ($navbar_page == "connections") ? 'navigation-link-current-page' : '' }}" href="{{ route("connections") }}">Connections</a>
+                </li>
+                <div class="d-xl-inline d-none" style="font-weight: 700;">|</div>
+                <li class="nav-item d-none d-xl-inline">
+                    <a class="nav-link navigation-link {{ ($navbar_page == "payments") ? 'navigation-link-current-page' : '' }}" href="{{ route("payments") }}">Payments</a>
+                </li>
+                <div class="d-xl-inline d-none" style="font-weight: 700;">|</div>
+                <li class="nav-item d-none d-xl-inline">
+                    <a class="nav-link navigation-link {{ ($navbar_page == "connections") ? 'navigation-link-current-page' : '' }}" href="{{ route("connections") }}">Water</a>
                 </li>
                 {{-- My Account Page
                     <div class="d-lg-inline d-none" style="font-weight: 700;">|</div>
@@ -30,18 +38,17 @@
                         <a class="nav-link navigation-link {{ ($navbar_page == "my account") ? 'navigation-link-current-page' : '' }}" href="{{ route("$mode.my account") }}">My Account</a>
                     </li>
                 --}}
-                {{-- Dropdown Example
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                <li class="nav-item dropdown d-xl-none">
+                <a class="nav-link navigation-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Services
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="nav-link navigation-link {{ ($navbar_page == "connections") ? 'navigation-link-current-page' : '' }}" href="{{ route("connections") }}">Connections</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="nav-link navigation-link {{ ($navbar_page == "payments") ? 'navigation-link-current-page' : '' }}" href="{{ route("payments") }}">Payments</a>
+                    <a class="nav-link navigation-link {{ ($navbar_page == "connections") ? 'navigation-link-current-page' : '' }}" href="{{ route("connections") }}">Water</a>
                 </div>
-                </li> --}}
+                </li>
                 <div class="d-xs-block d-lg-none">
                     @include('partials.navbar-account')
                 </div>
